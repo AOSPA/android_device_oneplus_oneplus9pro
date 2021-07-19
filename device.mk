7356d7985753f16fcb95c30bc82ff4c92f1040fe
Vishalcj17 <vishalcj17@gmail.com>
2021-07-19 23:16:00 +0530

    op9p: Initial OnePlus 9 Pro configuration.
     * This is an extension of OnePlus 9 tree
       to support OnePlus 9 Pro out of overlayFS.
    
    Change-Id: I84c4b63e2599b4f313f99dcb6827807b4f50e2f6

---
+++ device.mk
@@ -0,0 +1,20 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, vendor/oneplus/oneplus9pro/oneplus9pro-vendor.mk)
+$(call inherit-product, device/oneplus/oneplus9/device.mk)
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/oneplus9pro
