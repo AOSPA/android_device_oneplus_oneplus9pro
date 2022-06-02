40e429c33faea6a901f0544628888523fd76d93d
Alexander Koskovich <akoskovich@pm.me>
2022-06-01 17:00:44 -0700

    lemonadep: Move audio configurations to device tree.
     * As of LE2125_11_C.48.
    
    Change-Id: Ief2f6860b4ac0e400b196938a1ca90fb5ecae241

--- device.mk
+++ device.mk
@@ -14,6 +14,13 @@
 
 $(call inherit-product, vendor/oneplus/oneplus9pro/oneplus9pro-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
+
 # LTPO display on the OnePlus 9 Pro doesn't have apparent gamma shifts
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=250 \
